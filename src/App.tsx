ed3c16b58468524f5e61c63ed0a746303832904c
nastima <Grozdilova@mail.ru>
2025-07-28 16:33:42 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h3>
+      Человека определяют не заложенные в нем качества, а только его выбор©
+      </h3>
+    </div>  
+  )
+    
+}
+
+export default App
